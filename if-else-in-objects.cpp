d8d5ad37710641a8cddebe72a655e204cb211c53
dhilip0707 <dhilipkumar.dili@gmail.com>
2025-01-25 12:25:38 +0530

    basic-program

---
+++ if-else-in-objects.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class profile
+{
+  public:
+  string companyname;
+  string companyaddress;
+  string name;
+  string location;
+  int code;
+  int age;
+
+ 
+  void isadult()
+  {
+   if(age>=18)
+   {
+   cout<<"So the person is Adult" <<endl;
+   }
+   else
+   {
+    cout << "So the person is Child" << endl;
+   }  
+  }
+  void employeeDetails()
+  {
+   cout<<"The employee name is : " << name <<"\n"<<"The employee location is:"<<location<<"\n"<<"The employee code is:"<<code<<"\n"<<"The employee age is:"<<age<<"\n";
+  }
+  void companyDetails()
+  {
+   cout<<companyname<<" "<<companyaddress<<endl;
+  }
+  
+  void increase(int a)
+  {
+    age=age+a;
+  }
+  
+};
+int main()
+{
+  profile employee;
+  cout<<"Enter Employee name     :";
+  cin>>employee.name;
+  cout<<"Enter Employee location :";
+  cin>>employee.location;
+  cout<<"Enter Employee code	:";
+  cin>>employee.code;
+  cout<<"Enter Employee age	:";
+  cin>>employee.age;
+  cout<<"Enter the company name  :";
+  cin>>employee.companyname;
+  cout<<"Enter company address   :";
+  cin>>employee.companyaddress;
+  employee.employeeDetails();
+  employee.companyDetails();
+  employee.isadult();
+  employee.increase(5);
+  cout << "After 5 years the person's age is: " << employee.age << endl;
+  employee.isadult();
+}
---
+++ multiple-objects.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class student
+{
+public:
+string name;
+int std;
+int mark;
+
+void display()
+{
+cout << "The person name is: " << name << endl ;
+cout << "The person standard is: " << std << endl;
+cout << "The person mark is: "<< mark << endl;
+}
+
+
+};
+
+
+int main()
+{
+ student student1;
+ cout<<"Enter the name :";
+ cin>>student1.name;
+ cout<<"Enter the class:";
+ cin>>student1.std;
+ cout<<"Enter the mark :";
+ cin>>student1.mark;
+ 
+ student student2;
+ cout<<"Enter the name :";
+ cin>>student2.name;
+ cout<<"Enter the class:";
+ cin>>student2.std;
+ cout<<"Enter the mark :";
+ cin>>student2.mark;
+ 
+ student student3;
+ cout<<"Enter the name :";
+ cin>>student3.name;
+ cout<<"Enter the class:";
+ cin>>student3.std;
+ cout<<"Enter the mark :";
+ cin>>student3.mark;
+ 
+ student1.display();
+ student2.display();
+ student3.display();
+ 
+ /*cout<<student1.name<<"\n";
+ cout<<student2.name<<"\n";
+ cout<<student3.name<<"\n";*/
+ 
+}
+
